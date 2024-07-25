72afca35e55c2465cef3e7c373df5c2d873ff29a
AexoeZ <149087558+aexoez-codes@users.noreply.github.com>
2024-07-25 20:26:54 +0800

    Create Jep.sol
    This is a Remix Code for Metacrafters Module 3 Project using ERC20

---
+++ Jep.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/access/Ownable.sol";
+
+contract Jep is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Jep", "UIL") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    // Function to mint tokens
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Function to burn tokens
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    // Custom transfer function (not necessary to override if only calling super)
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        return super.transfer(recipient, amount);
+    }
+}
